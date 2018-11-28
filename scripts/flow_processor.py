76a5a7f4d58c6cc74b4c81b28041de2ab5922308
Emanuela26 <xinchen.wang@helix.com>
2018-11-28 15:00:07 -0800

    change file name

--- scripts/flow_processor.py
+++ scripts/flow_processor.py
@@ -8,20 +8,25 @@ class MyAddon:
         #construct HTTP response code
         self.http_code_ok = flowfilter.parse('~c 200')
         self.file_name = file + '.txt'
+        self.log = dict()
+        self.log['domains'] = set()
 
     # @concurrent 
     def request(self, flow):
         #log all host name in requests
         host = flow.request.host
+        log['domains'].add(host)
         with open(self.file_name,'a+') as f:
                     f.write(host + '\n')
+        if flow.request.content:
+            req_content = str(flow.request.content)
+            ctx.log('request content: %s' % req_content)
 
     def response(self, flow):
         if flowfilter.match(self.http_code_ok, flow):
             """when code is 200 ok"""
             if flow.response.content:
                 res_content = str(flow.response.content)
-                """Todo: regex to detect private data leak"""
                 #ctx.log('response content %s' % res_content)
                 
 
