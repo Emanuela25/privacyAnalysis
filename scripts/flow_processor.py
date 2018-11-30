6d7563e1a0c4a9a4cc80a73210327b65f4f81d74
Emanuela26 <xinchen.wang@helix.com>
2018-11-29 22:41:58 -0800

    added creating directory for log file

--- scripts/flow_processor.py
+++ scripts/flow_processor.py
@@ -8,7 +8,14 @@ class MyAddon:
     def __init__(self,file):
         #construct HTTP response code
         self.http_code_ok = flowfilter.parse('~c 200')
-        self.file_name = file + '.txt'
+        current_directory = os.getcwd()
+        print(current_directory)
+        self.folder_name = os.path.join(current_directory, 'logs', file)
+        print(self.folder_name)
+        if not os.path.exists(self.folder_name):
+                os.makedirs(self.folder_name, 0o777)
+            
+        self.url_file_name = self.folder_name + '/' + file + '_url.txt'
         self.whiltlist = ['127.0.0.1']
         self.host_set = set()
         
@@ -18,7 +25,7 @@ class MyAddon:
         host = flow.request.host
         if not host in self.host_set and not host in self.whiltlist:
             self.host_set.add(host)
-            with open(self.file_name,'a+') as f:
+            with open(self.url_file_name,'a+') as f:
                 f.write(host + '\n')
         if flow.request.content:
             req_content = str(flow.request.content)
