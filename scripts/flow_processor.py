3462bf4eb01b638ab823a223d3597979adf53415
Emanuela26 <xinchen.wang@helix.com>
2018-11-29 21:55:32 -0800

    added shell script for automatic run and changed flow processor

--- scripts/flow_processor.py
+++ scripts/flow_processor.py
@@ -1,34 +1,27 @@
 import os
 import sys
+import signal
 
 from mitmproxy import flowfilter, http, ctx
 
 class MyAddon:
-    def __init__(self, file):
+    def __init__(self,file):
         #construct HTTP response code
         self.http_code_ok = flowfilter.parse('~c 200')
         self.file_name = file + '.txt'
-        self.log = dict()
-        self.log['domains'] = set()
-
+        self.whiltlist = ['127.0.0.1']
+        self.host_set = set()
+        
     # @concurrent 
     def request(self, flow):
         #log all host name in requests
         host = flow.request.host
-        log['domains'].add(host)
-        with open(self.file_name,'a+') as f:
-                    f.write(host + '\n')
+        if not host in self.host_set and not host in self.whiltlist:
+            self.host_set.add(host)
+            with open(self.file_name,'a+') as f:
+                f.write(host + '\n')
         if flow.request.content:
             req_content = str(flow.request.content)
-            ctx.log('request content: %s' % req_content)
+            #ctx.log('request content: %s' % req_content)
 
-    def response(self, flow):
-        if flowfilter.match(self.http_code_ok, flow):
-            """when code is 200 ok"""
-            if flow.response.content:
-                res_content = str(flow.response.content)
-                #ctx.log('response content %s' % res_content)
-                
-
-
-addons = [ MyAddon(sys.argv[3]) ]
+addons = [MyAddon(sys.argv[3])]
---
+++ scripts/main.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+packname="com.duolingo"
+filename="${packname##*.}"
+#echo $filename
+mitmdump -s flow_processor.py $filename & 
+dump=$!
+monkey="$(adb shell monkey -p $packname --throttle 500 -v 100)"
+echo "${monkey}"
+kill -9 $dump
+
+
+
