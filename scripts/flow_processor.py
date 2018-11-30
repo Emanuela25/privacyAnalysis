0cb66098c739d7f7fa623b94e34b843492bb33d3
Emanuela26 <xinchen.wang@helix.com>
2018-11-29 23:27:11 -0800

    process get request query field

--- scripts/flow_processor.py
+++ scripts/flow_processor.py
@@ -8,7 +8,7 @@ class MyAddon:
     def __init__(self,file):
         #construct HTTP response code
         self.http_code_ok = flowfilter.parse('~c 200')
-        
+
         current_directory = os.getcwd()
         self.folder_name = os.path.join(current_directory, 'logs', file)
         if not os.path.exists(self.folder_name):
@@ -20,12 +20,23 @@ class MyAddon:
         
     # @concurrent 
     def request(self, flow):
-        #log all host name in requests
+        #log all domain names in requests
         host = flow.request.host
         if not host in self.host_set and not host in self.whiltlist:
             self.host_set.add(host)
             with open(self.host_file_name,'a+') as f:
                 f.write(host + '\n')
+        
+        #process get request url 
+        if flow.request.method == "GET":
+            url = flow.request.url
+            if flow.request.query:
+                print(url)
+                for q in flow.request.query:
+                    print(q)
+            
+
+        #process post request content
         if flow.request.content:
             req_content = str(flow.request.content)
             #ctx.log('request content: %s' % req_content)
