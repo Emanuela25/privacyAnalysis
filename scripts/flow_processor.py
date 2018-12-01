38f40ab6965a50446bf70427fab8fd33c576efae
Emanuela25 <xinchen.wang@helix.com>
2018-11-30 22:56:34 -0800

    changed key words, whitelist and logic flow

--- scripts/flow_processor.py
+++ scripts/flow_processor.py
@@ -17,13 +17,16 @@ class MyAddon:
                 os.makedirs(self.folder_name, 0o777)
         #define all log file names
         self.host_file_name = self.folder_name + '/' + file + '_domains.txt'
-        self.privacy_leak_file_name = self.folder_name + '/' + file + '_info.txt'
+        self.get_leak_file_name = self.folder_name + '/' + file + '_get_info.txt'
+        self.post_leak_file_name = self.folder_name + '/' + file + '_post_info.txt'
         #helper data strucutre
         self.encode = {'identity', 'gzip', 'deflate', 'br'}
         self.host_set = set() #domain set
-        self.whiltlist = ['127.0.0.1']#domains known well, no need to record
+        #domains known well, no need to record
+        self.whiltlist = ['127.0.0.1', 'android.clients.google.com','play.googleapis.com']
         self.keywords = {'sdkVersion', 'sdkVersionName', 'osversion', 
-        'gms', 'deviceModel', 'deviceMake', 'language'}
+        'gms', 'deviceModel', 'deviceMake', 'language', 'geoip_country', 
+        '$model', 'ssid', 'mac_address' }
     
     ###helper functions
     #determine if a string is a json object or not
@@ -67,55 +70,74 @@ class MyAddon:
             with open(self.host_file_name,'a+') as f1:
                 f1.write(host + '\n')
         
-        #process get request url 
-        if flow.request.method == "GET":
-            url = flow.request.url
-            #check if query field has any sensitive keyword inside
-            if flow.request.query:
-                leaks = dict()
-                for q in flow.request.query:
-                    key = q
-                    value = flow.request.query[q]
-                    if key in self.keywords:
-                        if not key in leaks:
-                            leaks[key] = {value}
-                        elif not value in leaks[key]:
-                            leaks[key].add(value)
-                #write to log file if any leaks
-                if leaks:
-                    with open(self.privacy_leak_file_name,'a+') as f2:
-                        f2.write(flow.request.method + ' ' + url  + ": \n")
-                        for key in leaks:
-                            f2.write("\t" + key + ": ")
-                            for value in leaks[key]:
-                                f2.write(value + " ")
-                            f2.write("\n")
+            #process get request url 
+            if flow.request.method == "GET":
+                url = flow.request.url
+                #check if query field has any sensitive keyword inside
+                if flow.request.query:
+                    leaks = dict()
+                    for q in flow.request.query:
+                        key = q
+                        value = flow.request.query[q]
+                        if key in self.keywords:
+                            if not key in leaks:
+                                leaks[key] = {value}
+                            elif not value in leaks[key]:
+                                leaks[key].add(value)
+                    #write to log file if any leaks
+                    if leaks:
+                        with open(self.get_leak_file_name,'a+') as f2:
+                            f2.write(flow.request.method + ' ' + url  + ": \n")
+                            for key in leaks:
+                                f2.write("\t" + key + ": ")
+                                for value in leaks[key]:
+                                    f2.write(value + " ")
+                                f2.write("\n")
 
-        #process post request content
-        if flow.request.method == "POST":
-            if flow.request.raw_content:
-                print(flow.request.url)
-                req_content = ""
-                if flow.request.headers.get("Content-Encoding", ""):
-                    encode_type = flow.request.headers.get("Content-Encoding", "")
-                    if not encode_type in self.encode:
-                        req_content = ""
+            #process post request content
+            if flow.request.method == "POST":
+                if flow.request.raw_content:
+                    req_content = ""
+                    if flow.request.headers.get("Content-Encoding", ""):
+                        encode_type = flow.request.headers.get("Content-Encoding", "")
+                        if not encode_type in self.encode:
+                            req_content = ""
+                        else:
+                            req_content = str(flow.request.content)
                     else:
                         req_content = str(flow.request.content)
-                else:
-                    req_content = str(flow.request.content)
 
-                if req_content:
-                    req_content_str = unquote(req_content[2:-1])
-                    if self.isJson(req_content_str):
-                        cotent_obj = json.loads(req_content_str)
-                        pairs = self.parseJson(cotent_obj)
-                        for k in pairs.keys():
-                            print(k + ' ' + pairs[k])
-                    else:
-                        #print('request content: %s' % req_content_str)
-                        print("request content is string")
-        
+                    if req_content:
+                        url = flow.request.url
+                        req_content_str = str(unquote(req_content[2:-1]))
+                        if self.isJson(req_content_str):
+                            print(url)
+                            leaks = dict()
+                            cotent_obj = json.loads(req_content_str)
+                            pairs = self.parseJson(cotent_obj)
+                            for k in pairs.keys():
+                                print(k + ' ' + pairs[k])
+                                if k in self.keywords:
+                                    if not k in leaks:
+                                        leaks[k] = {pairs[k]}
+                                    elif not value in leaks[key]:
+                                        leaks[key].add(value)
+                            if leaks:
+                                with open(self.post_leak_file_name,'a+') as f3:
+                                    f3.write('Json ' + url  + ": \n")
+                                    for key in leaks:
+                                        f3.write("\t" + key + ": ")
+                                        for value in leaks[key]:
+                                            f3.write(value + " ")
+                                        f3.write("\n")
+                        else:
+                            for word in self.keywords:
+                                if req_content_str.find(word) != -1:
+                                    with open(self.post_leak_file_name,'a+') as f3:
+                                        f3.write('String ' + url  + ": \n")
+                                        f3.write(req_content_str +'\n')
+                                    break
+                                    
                     #print('request content: %s' % req_content)
 
 addons = [MyAddon(sys.argv[3])]
--- scripts/main.sh
+++ scripts/main.sh
@@ -5,7 +5,7 @@ filename="${packname##*.}"
 #echo $filename
 mitmdump -s flow_processor.py $filename & 
 dump=$!
-monkey="$(adb shell monkey -p $packname --throttle 500 -v 100)"
+monkey="$(adb shell monkey -p $packname --throttle 500 -v 200)"
 echo "${monkey}"
 kill -9 $dump
 
